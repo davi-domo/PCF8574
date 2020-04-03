e3db8183c1b5b95ca5f1893caa0eb4d55dd04ca7
DAVID <d.auvre@free.fr>
2020-04-03 18:16:35 +0200

    Update PCF8574.py
    add of the PCF8574 interupt on ORANGE PI

--- Python/PCF8574.py
+++ Python/PCF8574.py
@@ -6,27 +6,46 @@
 
 import smbus
 import time
+import os
+import subprocess
 
 # Get I2C bus
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(0)
 
-# PCF8574 address, 0x20(32)
+# PCF8574 address, 0x20(32) or 0x38 (56)
 #		0xFF(255)	All pins configured as inputs
-bus.write_byte(0x20, 0xFF)
+bus.write_byte(0x38, 0xFF)
 
-time.sleep(0.5)
+# PCF8474 interupt declaration of the pine of orange pi pc
+# PCF8574 INT wiring to physical pin 5 GPIO 6, PULL-UP resistance of 1khoms
+os.system('echo "6" > /sys/class/gpio/export')
+time.sleep(0.1)
+os.system('echo "in" > /sys/class/gpio/gpio6/direction')
+time.sleep(0.1)
 
-# PCF8574 address, 0x20(32)
-# Read data back, 1 byte
-data = bus.read_byte(0x20)
+# data acquisition variable
+etat_pin = [0,0,0,0,0,0,0,0]
 
+# infinite loop
+while 1 :
+    # Check the status of the interrupt
+    interupt = int(subprocess.check_output(["cat","/sys/class/gpio/gpio6/value"]))
+    # if change of state we read and store the state in the variable etat_pin
+    if interupt == 0 :
+        # PCF8574 address, 0x20(32) or 0x38 (56)
+        # Read data back, 1 byte
+        data = bus.read_byte(0x38)
 
-# Convert the data
-data = (data & 0xFF)
+        # Convert the data
+        data = (data & 0xFF)
 
-for i in range(0, 8) :
-    if (data & (2 ** i)) == 0 :
-        print "I/O Pin %d State is LOW" %(i)
-    else :
-        print "I/O Pin %d State is HIGH" %(i)
-        time.sleep(0.5)
+        for i in range(0, 8) :
+            #print (data & (2 ** i))
+            if (data & (2 ** i)) == 0 :
+                etat_pin[i] = 0
+            else :
+                etat_pin[i] = 1
+                time.sleep(0.01)
+        # for debug        
+        print (etat_pin)
+    time.sleep(0.5)
